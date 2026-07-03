9dfc35a4c1d9cc446f6542fb71662471f722a558
darkgamer91111-hue <dark.gamer91111@gmail.com>
2026-07-03 07:28:40 +0000

    Fix typos and update layout

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/LevelInfoLayer.hpp>
+
 using namespace geode::prelude;
+
 class $modify(MyLevelInfoLayer, LevelInfoLayer) {
     bool init(GJGameLevel* lvl, bool ch) {
         if (!LevelInfoLayer::init(lvl, ch)) return false;
-        double pct = lvl->m_normalPercent;
-        if (pct > 0.0) {
+        
+        int pct = lvl->m_normalPercent;
+        if (pct > 0) {
             auto lbl = typeinfo_cast<CCLabelBMFont*>(this->getChildByID("percentage-label"));
             if (lbl) {
-                std::string txt = numToFormatedString(pct, 2) + "%";
+                std::string txt = std::to_string(pct) + "%";
                 lbl->setString(txt.c_str());
             }
         }
